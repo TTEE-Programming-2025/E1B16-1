0b36c9ce8738dcba672a57bcd7434201077ea442
rainobow <rain32060311@gmail.com>
2025-05-12 01:30:29 +0800

    新增功能b

--- hw3.c
+++ hw3.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h> 
+#include<stdbool.h>
 
 void dot(char seat[9][9]){//turn all the element in seat into '.'
 	int i,j;
@@ -37,6 +38,73 @@ void show(char seat[9][9]){
 		printf("\n");
 	}
 
+}
+
+bool check(char seat[9][9],int row,int col, int num, bool h){
+	int i;
+		if(h){
+			if(col+num>9){
+				return false;}
+			for(i=0;i<num;i++){
+				if(seat[row][col+i]!='.'){return false;
+				}
+			}}
+		else{
+				if(row+num>9){
+					return false;
+				}
+				for(i=0;i<num;i++){
+					if(seat[row+1][col]!='.'){return false;
+					}
+				}
+			}
+			return true;
+			}
+			
+			
+			
+		
+
+bool arr(char seat[9][9],int num){
+	int i,j,k;
+	for(i=0;i<9;i++){
+		for(j=0;j<=9-num;j++){
+			if(check(seat,i,j,num,true)){
+				for(k=0;k<num;k++){
+					seat[i][j+k]='@';
+				}
+				return true;
+			}
+		}
+	}
+	
+	
+	for(j=0;j<9;j++){
+		for(i=0;i<=9-num;i++){
+		if(check(seat,i,j,num,false)){
+			for(k=0;k<num;k++){
+				seat[i+k][j]='@';
+			}
+			return true;
+		}
+	}
+}
+	
+	if(num==4){
+		for(i=0;i<8;i++){
+			for(j=0;j<8;j++){
+				if(seat[i][j]=='.'&& seat[i][j+1]=='.'&&
+				   seat[i+1][j]=='.'&& seat[i+1][j+1]=='.'){
+				   
+				   seat[i][j]='@';
+				   seat[i][j+1]='@';
+				   seat[i+1][j]='@';
+				   seat[i+1][j+1]='@';
+				   return true;
+			}
+			}
+		}
+	}
 }
 int main(void){
 	printf("                                   \n");
@@ -103,5 +171,56 @@ int main(void){
 		system("CLS");
 		goto manu;
 	}
-	return 0;
-}
+	
+	if(manu=='b'){
+		system("CLS");
+		char seat[9][9];
+		dot(seat);
+		random(seat);
+		
+		int b;
+		ask_b:
+		printf("how many seat you need(1~4)\n");
+		scanf("%d", &b);
+		if(b<1||b>4){
+			system("CLS");
+			printf("wrong\n");
+			goto ask_b;
+		}
+		
+		if(arr(seat,b)){
+			printf("\n arranged seat:\n");
+			show(seat);
+			
+			printf("\n are you satisfied? (y/n):");
+			char sb;
+			int i,j;
+			scanf(" %c",&sb);
+			if(sb=='y'){
+				for(i=0;i<9;i++){
+					for(j=0;j<9;j++){
+						if(seat[i][j]=='@'){
+							seat[i][j]='*';
+						}
+					}
+				}
+				system("CLS");
+				printf("yes~~~\n");
+				goto manu;
+			}
+			if(sb=='n'){
+				system("CLS");
+				goto manu;}
+		}
+		
+			
+		}
+		return 0;}
+		
+		
+		
+		
+		
+		
+	
+
