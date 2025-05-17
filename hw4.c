57f3b00174e465277f7d6631090424cc3f2688cd
rainobow <rain32060311@gmail.com>
2025-05-17 16:19:16 +0800

    新增功能介面和功能a

--- hw4.c
+++ hw4.c
@@ -42,5 +42,87 @@ int main(void){
 		if(wrong==3){
 		printf("掰掰");return 0;}}		
 	}while(wrong<3);
+	
+	m:
+	printf("\n|-------------[Grade System]---------------|");
+	printf("\n|                                          |");
+	printf("\n|---------a. Enter student grades----------|");
+	printf("\n|                                          |");
+	printf("\n|---------b. Display student grades--------|");
+	printf("\n|                                          |");
+	printf("\n|---------c. Search for student grades-----|");
+	printf("\n|                                          |");
+	printf("\n|---------d. Grade ranking-----------------|");
+	printf("\n|                                          |");
+	printf("\n|---------e. Exit system-------------------|");
+	printf("\n|                                          |");
+	printf("\n|------------------------------------------|");
+	printf("\n");
+	printf("輸入你要的選項：");
+	char choice;
+	scanf(" %c",&choice) ;
+	
+	a(choice);
+	system("CLS");
+	goto m;
+	
+	
+	
+	
 	return 0;
 }
+void a(int a){
+	if(a=='a'){
+	system("CLS");
+		printf("請輸入一整數值n介於5~10：");
+		
+		
+		 
+		int n,i;
+		scanf("%d",&n);
+		if(n>=5&&n<=10){
+			struct a{
+				char name[10];
+				int number;
+				int math;
+				int phy;
+				int eng;
+		 	} s[n];
+		 
+			for(i=0;i<n;i++){
+				printf("輸入學生%d 姓名：",i+1);
+				scanf(" %s",s[i].name);
+				number:
+				printf("輸入該學生學號：");
+				scanf("%d",&s[i].number);
+				if(s[i].number>999999||s[i].number<0){
+					printf("wrong,please enter again\n");
+					goto number;
+				}
+				math:
+				printf("輸入該學生數學成績：");
+				scanf("%d",&s[i].math);
+				if(s[i].math>100||s[i].math<0){
+					printf("wrong,please enter again\n");
+					goto math;
+				}
+				psy:
+				printf("輸入該學生物理成績：");
+				scanf("%d",&s[i].phy);
+				if(s[i].phy>100||s[i].phy<0){
+					printf("wrong,please enter again\n");
+					goto psy;
+				}
+				eng:
+				printf("輸入該學生英文成績：");
+				scanf("%d",&s[i].eng);
+				if(s[i].eng>100||s[i].eng<0){
+					printf("wrong,please enter again\n");
+					goto eng;
+				}
+				
+				printf("\n");
+			}
+		}
+	
+}}
